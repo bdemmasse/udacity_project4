a5e27b3502cf6e6519f5d586a482286919bbe2ef
raphealolams <raphealolams@gmail.com>
2020-01-23 11:56:39 +0100

    kubernetes log added

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,14 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="raphaelolams/api"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-$(aws ecr get-login --no-include-email --region us-east-2)
-docker build -t project_4 .
-docker tag project_4:latest 931162216800.dkr.ecr.us-east-2.amazonaws.com/project_4:latest
+
+docker login
+docker tag api:latest $dockerpath:latest
+
 
 # Step 3:
 # Push image to a docker repository
-docker push 931162216800.dkr.ecr.us-east-2.amazonaws.com/project_4:latest
+docker push $dockerpath:latest
